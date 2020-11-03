1571a49d3b05c338c659f3ee7021e44b358f8c8b
AbhipsaGuru1012 <58511560+AbhipsaGuru1012@users.noreply.github.com>
2020-11-03 15:23:30 +0530

    Update comments.js

--- routes/comments.js
+++ routes/comments.js
@@ -5,7 +5,7 @@ const Comment=require("../models/comments");
 const middleware=require("../middleware");
 //COMMENTS ROUTE-Nested Routes
 router.get("/new",middleware.isLoggedIn,function(req,res){
-	//find campground by id
+	
 	Campground.findById(req.params.id, function(err,campground){
 		if(err){
 			req.flash("error", "Something went wrong!")
@@ -18,30 +18,30 @@ router.get("/new",middleware.isLoggedIn,function(req,res){
 	
 })
 router.post("/",middleware.isLoggedIn, function(req, res){
-	//lookup campground with the id
+	
 	Campground.findById(req.params.id, function(err, campground){
 		if(err){
 			req.flash("error", "Something went wrong!")
 			console.log(err)
 			res.redirect("/campgrounds")
 		}else{
-			//create a new comment
-			Comment.create(req.body.comment,function(err, comment){//body.comment has both text and author as we used like an object in new.ejs
+			
+			Comment.create(req.body.comment,function(err, comment){
 				if(err){
 					req.flash("error", "Something went wrong!")
 					console.log(err)
 					
 				}else{
-					//add username and id to comment
+					
 					comment.author.id=req.user._id;
 					comment.author.username=req.user.username;
-					//save comment
+					
 					comment.save();
-					campground.comments.push(comment)//here campground is the parameter in the function that we searched for
-					campground.save()
+					campground.comments.push(comment);
+					campground.save();
 					console.log(comment);
-					req.flash("Successfully added comment")
-					res.redirect("/campgrounds/"+campground._id)
+					req.flash("Successfully added comment");
+					res.redirect("/campgrounds/"+campground._id);
 				}
 			})
 		}
@@ -49,9 +49,9 @@ router.post("/",middleware.isLoggedIn, function(req, res){
 	
 })
 
-//Comment edit route
+
 router.get("/:comment_id/edit", middleware.checkCommentOwnership, function(req, res){
-	Campground.findById(req.params.id, function(err, foundCampground){//only writing this to check if the id of campground is right, or else our app could crash
+	Campground.findById(req.params.id, function(err, foundCampground){
 		if(err || !foundCampground){
 			req.flash("error", "Campground not found!");
 			return res.redirect("back");
@@ -68,7 +68,7 @@ router.get("/:comment_id/edit", middleware.checkCommentOwnership, function(req, 
 	
 	
 })
-//Update comment router
+
 router.put("/:comment_id", middleware.checkCommentOwnership, function(req, res){
 	Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
 		if(err){
@@ -79,7 +79,7 @@ router.put("/:comment_id", middleware.checkCommentOwnership, function(req, res){
 	})
 })
 
-//Delete comment route
+
 router.delete("/:comment_id", middleware.checkCommentOwnership, function(req, res){
 	Comment.findByIdAndRemove(req.params.comment_id, function(err){
 		if(err){
