abac4544cc8189351d39fd6c1e56c54db9fa3a04
AbhipsaGuru1012 <58511560+AbhipsaGuru1012@users.noreply.github.com>
2020-11-03 15:10:19 +0530

    initial commit

--- middleware/index.js
+++ middleware/index.js
@@ -10,12 +10,12 @@ middlewareObj.checkCampgroundOwnership=function(req, res, next){
 	if(req.isAuthenticated()){//to see if logged in
 		
 		Campground.findById(req.params.id, function(err, foundCampground){
-			if(err  || !foundCampground){//!foundCampground(returns true) is id we enter some random or invalid id in url
+			if(err  || !foundCampground){
 				req.flash("error", "Couldn't find the Campground you are looking for")
 				res.redirect("back");
 			}else{
 				//does the user own the campground
-				if(foundCampground.author.id.equals(req.user._id)){//mongoose provides .equals function, check it the owner's id is equal to currently logged in user's id
+				if(foundCampground.author.id.equals(req.user._id)){
 					next();
 				}else{
 					req.flash("error", "You don't have permission to do that")
@@ -29,12 +29,12 @@ middlewareObj.checkCampgroundOwnership=function(req, res, next){
 	
 	}else{
 		req.flash("error", "You need to be logged in to do that")
-		res.redirect("back");//redirect back to where they came from
+		res.redirect("back");
 	}
 	
 }
 middlewareObj.checkCommentOwnership=function(req, res, next){
-	//is user logged in
+	
 	if(req.isAuthenticated()){
 		
 		Comment.findById(req.params.comment_id, function(err, foundComment){
@@ -42,8 +42,8 @@ middlewareObj.checkCommentOwnership=function(req, res, next){
 				req.flash("error", "Comment not found!")
 				res.redirect("back");
 			}else{
-				//does the user own the comment
-				if(foundComment.author.id.equals(req.user._id)){//mongoose provides .equals function, check it the owner's id is equal to currently logged in user's id
+				
+				if(foundComment.author.id.equals(req.user._id)){
 					next();
 				}else{
 					req.flash("error", "You don't have permission to do that")
@@ -57,11 +57,11 @@ middlewareObj.checkCommentOwnership=function(req, res, next){
 	
 	}else{
 		req.flash("error", "You need to be logged in")
-		res.redirect("back");//redirect back to where they came from
+		res.redirect("back");
 	}
 }
 middlewareObj.checkReviewOwnership=function(req, res, next){
-	//is user logged in
+	
 	if(req.isAuthenticated()){
 		
 		Review.findById(req.params.reviewId, function(err, foundReview){
@@ -71,8 +71,8 @@ middlewareObj.checkReviewOwnership=function(req, res, next){
 				req.flash("error", "Review not found!")
 				res.redirect("back");
 			}else{
-				//does the user own the review
-				if(foundReview.author.id.equals(req.user._id)){//mongoose provides .equals function, check it the owner's id is equal to currently logged in user's id
+				
+				if(foundReview.author.id.equals(req.user._id)){
 					next();
 				}else{
 					req.flash("error", "You don't have permission to do that")
@@ -86,7 +86,7 @@ middlewareObj.checkReviewOwnership=function(req, res, next){
 	
 	}else{
 		req.flash("error", "You need to be logged in")
-		res.redirect("back");//redirect back to where they came from
+		res.redirect("back");
 	}
 }
 
@@ -95,7 +95,7 @@ middlewareObj.isLoggedIn=function(req, res, next){
 	if(req.isAuthenticated()){
 		return next();
 	}
-	req.flash("error", "You need to login first");//need to place it before redirect
+	req.flash("error", "You need to login first");
 	res.redirect("/login")
 
 }
