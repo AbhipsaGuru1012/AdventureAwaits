f6cee00472c0fc384c2369108d4d42b9ad0269c2
AbhipsaGuru1012 <58511560+AbhipsaGuru1012@users.noreply.github.com>
2020-11-03 15:21:22 +0530

    Update campgrounds.js

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -7,16 +7,16 @@ const mbxGeocoding=require("@mapbox/mapbox-sdk/services/geocoding");
 const mapboxToken=process.env.MAPBOX_TOKEN;
 const geocoder=mbxGeocoding({accessToken:mapboxToken});
 
-//multer congiguration for image upload
+
 const multer = require('multer');
-const storage = multer.diskStorage({//the image being uploaded is stored in a var name
+const storage = multer.diskStorage({
   filename: function(req, file, callback) {
-    callback(null, Date.now() + file.originalname);//the date and file name
+    callback(null, Date.now() + file.originalname);
   }
 });
 const imageFilter = function (req, file, cb) {
-    // accept image files only
-    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {//must have extensions in the image file
+   
+    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
         return cb(new Error('Only image files are allowed!'), false);
     }
     cb(null, true);
@@ -26,13 +26,13 @@ const upload = multer({ storage: storage, fileFilter: imageFilter})
 const cloudinary = require('cloudinary');
 cloudinary.config({ 
   cloud_name: 'dvtlx8b6h', 
-  api_key: process.env.CLOUDINARY_API_KEY, //store it in .env file and hide from others, don't upload env files in github
+  api_key: process.env.CLOUDINARY_API_KEY, 
   api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 
 router.get("/",function(req,res){
-	//Get all camgrounds from DB
+	
 	Campground.find({},function(err,campgrounds){
 		if(err){
 			console.log(err);
@@ -49,7 +49,7 @@ router.get("/new",middleware.isLoggedIn,function(req,res){
 	res.render("campgrounds/new");
 })
 
-router.post("/", middleware.isLoggedIn, upload.single('image'),async function(req,res){//image is coming from the 'new' form and here the upload is a middleware
+router.post("/", middleware.isLoggedIn, upload.single('image'),async function(req,res){
 	const geoData=await geocoder.forwardGeocode({
 		query:req.body.campground.location,
 		limit:1
@@ -63,9 +63,9 @@ router.post("/", middleware.isLoggedIn, upload.single('image'),async function(re
 			req.flash('error', err.message);
 			return res.redirect("back");
 		}
-		  // add cloudinary url for the image to the campground object under image property
-		  req.body.campground.image = result.secure_url;//adding image field to campground
-		//add images's public id to campground object
+		  
+		  req.body.campground.image = result.secure_url;
+		
 		  req.body.campground.imageId=result.public_id;
 		  // add author to campground
 		  req.body.campground.author = {
@@ -73,27 +73,12 @@ router.post("/", middleware.isLoggedIn, upload.single('image'),async function(re
 			username: req.user.username
 		  }
 		
-
-		// var name=req.body.name;
-		// var price=req.body.price;
-		// var image=req.body.image;
-		// var desc=req.body.description
-		// var author={
-		// 	id:req.user._id,
-		// 	username:req.user.username
-		// }
-		// var newCampground={name:name, price:price,image:image, description:desc, author:author}
-
-
-
-		// console.log(req.user);
-		//create a new campground and save to DB
 		Campground.create(req.body.campground, function(err,newlyCreated){
 			if(err){
 				req.flash('error', err.message);
 				return res.redirect("back");
 			}else{
-				// console.log(newlyCreated)
+				
 				req.flash("success","Added a new campground!")
 				res.redirect("/campgrounds");
 			}
@@ -102,17 +87,16 @@ router.post("/", middleware.isLoggedIn, upload.single('image'),async function(re
 	
 })
 
-//Shows more info about one campground
+
 router.get("/:id", function(req, res){
-	//FIND THE CAMPGROUND WITH THE PARTICULAR ID
-	Campground.findById(req.params.id).populate("comments").exec(async function(err, foundCampground){//populating the comments array on it so that it's not just an id
+	
+	Campground.findById(req.params.id).populate("comments").exec(async function(err, foundCampground){
 		if(err || !foundCampground){
 			req.flash("error", "Couldn't find the campground you are looking for");
 			res.redirect("back");
 		}else{
-			await foundCampground.populate('review').execPopulate();//need to call execPopulate to actually populate it
-			// console.log(foundCampground);
-			//render the show template for that campground
+			await foundCampground.populate('review').execPopulate();
+			
 			res.render("campgrounds/show", {campground:foundCampground});
 		}
 	})
@@ -131,9 +115,9 @@ router.get("/:id/edit", middleware.checkCampgroundOwnership, function(req, res){
 })
 
 
-//Update Route
+
 router.put("/:id", middleware.checkCampgroundOwnership, upload.single('image'), function(req, res){
-	Campground.findById(req.params.id, async function(err, campground){//earlier it was just findByIdAndUpdate with middle parameter as req.body.campground and err and the simple else part/async bec the image upload can take longer and other statements get executed first
+	Campground.findById(req.params.id, async function(err, campground){
 		const geoData=await geocoder.forwardGeocode({
 			query:req.body.campground.location,
 			limit:1
@@ -142,18 +126,18 @@ router.put("/:id", middleware.checkCampgroundOwnership, upload.single('image'), 
 			req.flash('error', err.message);
 			res.redirect("/campgrounds")
 		}else{
-			if(req.file){//if an image is uploaded/changed
+			if(req.file){
 				try{
-					await cloudinary.v2.uploader.destroy(campground.imageId);//destroy prev image//await means wait till the line gets executed
+					await cloudinary.v2.uploader.destroy(campground.imageId);
 					
 					var result=await cloudinary.v2.uploader.upload(req.file.path);//upload new image
 					campground.imageId=result.public_id;
 					campground.image=result.secure_url;
-					// campground.image.push(result);//not overwriting prev images but pushing them
+					
 				}
-				catch(err){//catch the error
+				catch(err){
 					req.flash('error', err.message);
-					return res.redirect("/campgrounds")//return means exit from the whole block
+					return res.redirect("/campgrounds");
 					
 				}
 					
@@ -164,22 +148,22 @@ router.put("/:id", middleware.checkCampgroundOwnership, upload.single('image'), 
 			campground.location=req.body.campground.location;
 			campground.price=req.body.campground.price;
 			campground.geometry=geoData.body.features[0].geometry;
-			campground.save();//updated and now save in db
+			campground.save();
 			req.flash("success", "Successfully updated!");
 			res.redirect("/campgrounds/"+req.params.id);
 		}
 	})
 })
-//delete campground
+
 router.delete("/:id", middleware.checkCampgroundOwnership, function(req, res){
-	Campground.findById(req.params.id, async function(err, campground){//earlier it was simply findByIdAndRemove
+	Campground.findById(req.params.id, async function(err, campground){
 		if(err){
 			req.flash('error', err.message);
 			return res.redirect("back")
 			
 		}
 		try{
-			await cloudinary.v2.uploader.destroy(campground.imageId);//destroy prev image//await means wait till the line gets executed
+			await cloudinary.v2.uploader.destroy(campground.imageId);
 			campground.remove();
 			req.flash("success", "Successfully deleted the campground")
 			res.redirect("/campgrounds")
@@ -192,7 +176,7 @@ router.delete("/:id", middleware.checkCampgroundOwnership, function(req, res){
 	
 })
 
-//Adding review routes
+
 router.post("/:id/reviews", middleware.isLoggedIn, async function(req,res){
 	const campground=await Campground.findById(req.params.id);
 	const review=new Review(req.body.review);
@@ -205,11 +189,11 @@ router.post("/:id/reviews", middleware.isLoggedIn, async function(req,res){
 	await review.save();
 	await campground.save();
 	req.flash("success", "Created a new Review!");
-	res.redirect(`/campgrounds/${campground._id}`)//adding `` is string template literal technique
+	res.redirect(`/campgrounds/${campground._id}`)
 })
 router.delete("/:id/reviews/:reviewId", middleware.isLoggedIn, middleware.checkReviewOwnership, async function(req, res){
 	const {id, reviewId}=req.params;
-	Campground.findByIdAndUpdate(id, {$pull:{review:reviewId}});//pull is a mongoose method where we are pulling out the reviewId out of reviews(check)
+	Campground.findByIdAndUpdate(id, {$pull:{review:reviewId}});
 	await Review.findByIdAndDelete(reviewId);
 	req.flash("success", "Deleted the review successfully")
 	res.redirect(`/campgrounds/${id}`);
