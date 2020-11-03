542bc63b1aedc87433e9dd484b185fbae98342ee
AbhipsaGuru1012 <58511560+AbhipsaGuru1012@users.noreply.github.com>
2020-11-03 15:24:45 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -7,18 +7,17 @@ router.get("/",function(req,res){
 	
 	res.render("landing");
 })
-//Auth Routes
-//show register form
+
 router.get("/register", function(req, res){
 	res.render("register");
 })
-//handle sign-up logic
+
 router.post("/register",function(req,res){
-	const newUser=new User({username:req.body.username});//newUser has only the username assigned
-	User.register(newUser, req.body.password, function(err, user){//user is the newUser that was made
+	const newUser=new User({username:req.body.username});
+	User.register(newUser, req.body.password, function(err, user){
 		if(err){
 			console.log(err);
-			//whatever error message we get in the error ie err here
+			
 			return res.render("register", {"error": err.message});
 		}
 		passport.authenticate("local")(req, res, function(){
@@ -27,18 +26,18 @@ router.post("/register",function(req,res){
 		})
 	})
 })
-//login form
+
 router.get("/login", function(req, res){
 	res.render("login");
 })
-//login logic
+
 router.post("/login", passport.authenticate("local",
 	{
 		successRedirect:"/campgrounds",
 		failureRedirect:"/login"
 	}), function(req, res){
 })
-//logout route
+
 router.get("/logout", function(req,res){
 	req.logout();
 	req.flash("success", "Logged you out successfully!")
